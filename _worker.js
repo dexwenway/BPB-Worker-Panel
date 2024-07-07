5eb96838080dc9739e341c2c939ec1635798d474
dexwenway <127918479+dexwenway@users.noreply.github.com>
2024-07-07 14:45:49 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '25bdfa30-4cef-463b-9583-c5d0dad5d792';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
